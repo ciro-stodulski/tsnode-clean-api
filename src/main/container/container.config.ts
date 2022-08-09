299ef4c358c87c264e8d3a91cf1e64646f109f86
ciro.azevedo <ciro.azevedo@4all.com>
2022-08-08 22:33:00 -0300

    fix lint

--- src/main/container/container.config.ts
+++ src/main/container/container.config.ts
@@ -4,11 +4,11 @@ import { KnexAdapter } from '../../infra/adapters';
 import { env, logger } from '../../shared';
 
 export class ContainerConfig {
-   readonly db: KnexAdapter;
+  readonly db: KnexAdapter;
 
-   readonly client_cache: CacheClient;
+  readonly client_cache: CacheClient;
 
-   readonly amqp_client: AmqpClient;
+  readonly amqp_client: AmqpClient;
 
   constructor() {
     this.db = new KnexAdapter();
--- src/main/container/factories/services.ts
+++ src/main/container/factories/services.ts
@@ -1,10 +1,10 @@
 import { TodoService } from '../../../infra/services';
 import { ServiceCaseContext, InfraContext } from '.';
 
-export const makeServiceContext = (
+export const make_service_context = (
   infra_context: InfraContext
 ): ServiceCaseContext => {
   return {
-    todo_service: new TodoService(infra_context)
-  }
+    todo_service: new TodoService(infra_context),
+  };
 };
