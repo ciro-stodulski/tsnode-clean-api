1dd0514f764bffa3842a60a20cd8bfc9020eadea
ciro-stodulski <ciro.sda@gmail>
2022-09-10 18:20:25 -0300

    VAE

--- scripts/kubernetes/builder-deployments.sh
+++ scripts/kubernetes/builder-deployments.sh
@@ -24,8 +24,8 @@ sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   deployment.yaml
 sed -i  "s/\$APP_VERSION/${APP_VERSION}/"     deployment.yaml
 sed -i  "s/\$NAMESPACE/${NAMESPACE}/"     deployment.yaml
 sed -i  "s/\$DOCKER_IMAGE/${APP_IMAGE_REPO}\/${APP_IMAGE}:${APP_IMAGE_VERSION}/"     deployment.yaml
-sed -i  "s/\$SERVICEPORT/3000/"     deployment.yaml
-
+sed -i  "s/\$SERVICEPORT/${SERVICEPORT}/"     deployment.yaml
+sed -i  "s/\$SERVICEPORT/${SERVICEPORT}/"  deployment.yaml
 
 sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   hpa.yaml
 sed -i  "s/\$NAMESPACE/${NAMESPACE}/"     hpa.yaml
@@ -33,3 +33,4 @@ sed -i  "s/\$NAMESPACE/${NAMESPACE}/"     hpa.yaml
 
 sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   service.yaml
 sed -i  "s/\$NAMESPACE/${NAMESPACE}/"     service.yaml
+sed -i  "s/\$SERVICEPORT/${SERVICEPORT}/"  service.yaml
