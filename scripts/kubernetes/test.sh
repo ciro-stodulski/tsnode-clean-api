78c4d94096c9a728532adaee471db12ad460b4a4
ciro-stodulski <ciro.sda@gmail>
2022-09-10 15:18:40 -0300

    goo

--- scripts/kubernetes/test.sh
+++ scripts/kubernetes/test.sh
@@ -18,21 +18,18 @@ function readJson {
 }
 
 APP_VERSION=`readJson ../../package.json version` || exit 1;
-NAME=`readJson ../../package.json name` || exit 1;
+NAMESPACE=`readJson ../../package.json namespace` || exit 1;
 
-DOCKER_USERNAME=${{inputs.docker_username}}
-DOCKER_VERSION=$(git rev-parse --short "$GITHUB_SHA")
-
-sed -i  "s/\$PROJECT_NAME/${NAME}/"   deployment.yaml
+sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   deployment.yaml
 sed -i  "s/\$APP_VERSION/${APP_VERSION}/"     deployment.yaml
 sed -i  "s/\$NAMESPACE/lab/"     deployment.yaml
-sed -i  "s/\$DOCKER_IMAGE/${DOCKER_USERNAME}/${NAME}:${DOCKER_VERSION}/"     deployment.yaml
+sed -i  "s/\$DOCKER_IMAGE/${APP_IMAGE_REPO}/${APP_IMAGE}:${APP_IMAGE_VERSION}/"     deployment.yaml
 sed -i  "s/\$SERVICEPORT/3000/"     deployment.yaml
 
 
-sed -i  "s/\$PROJECT_NAME/${NAME}/"   hpa.yaml
+sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   hpa.yaml
 sed -i  "s/\$NAMESPACE/lab/"     hpa.yaml
 
 
-sed -i  "s/\$PROJECT_NAME/${NAME}/"   service.yaml
-sed -i  "s/\$NAMESPACE/lab/"     service.yaml
+sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   service.yaml
+sed -i  "s/\$NAMESPACE/${NAMESPACE}/"     service.yaml
