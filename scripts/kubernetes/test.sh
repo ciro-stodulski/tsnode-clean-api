3704f4456802c9efcaa86331f71588b4079c460d
ciro-stodulski <ciro.sda@gmail>
2022-09-10 15:07:19 -0300

    fix

--- scripts/kubernetes/test.sh
+++ scripts/kubernetes/test.sh
@@ -20,13 +20,13 @@ function readJson {
 APP_VERSION=`readJson ../../package.json version` || exit 1;
 NAME=`readJson ../../package.json name` || exit 1;
 
-DOCKER_USERNAME=$(echo toJSON(inputs.docker_username))
+DOCKER_USERNAME=${{inputs.docker_username}}
 DOCKER_VERSION=$(git rev-parse --short "$GITHUB_SHA")
 
 sed -i  "s/\$PROJECT_NAME/${NAME}/"   deployment.yaml
 sed -i  "s/\$APP_VERSION/${APP_VERSION}/"     deployment.yaml
 sed -i  "s/\$NAMESPACE/lab/"     deployment.yaml
-sed -i  "s/\$DOCKER_IMAGE/${DOCKER_USERNAME)}/${NAME}:$(git rev-parse --short "${DOCKER_VERSION}")/"     deployment.yaml
+sed -i  "s/\$DOCKER_IMAGE/${DOCKER_USERNAME}/${NAME}:$(git rev-parse --short "${DOCKER_VERSION}")/"     deployment.yaml
 sed -i  "s/\$SERVICEPORT/3000/"     deployment.yaml
 
 
